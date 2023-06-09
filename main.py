9bfd5209b3f829ec6e0ab8086b3db17de89f306f
raz <razie.razavi1985@gmail.com>
2023-06-09 04:39:50 +0330

    Learned loops

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy
+
+razavi_children = ['Razie', 'Morteza', 'Mostafa', 'Pooskshooloo']
+
+print('Total number of children:', len(razavi_children))
+
+
+print('\nWithout loop (most stupid)')
+print(razavi_children[0])
+print(razavi_children[1])
+print(razavi_children[2])
+print(razavi_children[3])
+
+
+print('\nUsing loop with index (not best)')
+for i in range(len(razavi_children)):
+    print(razavi_children[i])
+
+
+print('\nUsing loop without index (best way)')
+for child in razavi_children: 
+    print(child)
